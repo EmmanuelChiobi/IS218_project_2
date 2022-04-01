d1e0bc92656c8ee3b9418bd160e6c4974dbb2fab
Emmanuel Chiobi <ec362@njit.edu>
2022-04-01 02:32:01 -0400

    More files added

--- calculator/operations/__init__.py
+++ calculator/operations/__init__.py
@@ -17,7 +17,7 @@ class Subtraction:
 
     @staticmethod
     def subtract(value_1, value_2):
-        """ This is the add method"""
+        """ This is the subtract method"""
         return value_1 - value_2
 
 
@@ -26,5 +26,13 @@ class Multiplication:
 
     @staticmethod
     def multiply(value_1, value_2):
-        """ This is the add method"""
+        """ This is the multiply method"""
         return value_1 * value_2
+
+class Division:
+    """ This is the division class """
+
+    @staticmethod
+    def divide(value_1, value_2):
+        """ This is the divide method"""
+        return value_1 / value_2
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -8,6 +8,10 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/python">Python</a>' in response.data
     assert b'<a class="nav-link" href="/cicd">CI/CD</a>' in response.data
+    assert b'<a class="nav-link" href="/pylint">Pylint</a>' in response.data
+    assert b'<a class="nav-link" href="/aaa">AAA</a>' in response.data
+    assert b'<a class="nav-link" href="/oop">OOP</a>' in response.data
+    assert b'<a class="nav-link" href="/solid">Solid</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
@@ -36,6 +40,30 @@ def test_request_python(client):
 def test_request_cicd(client):
     """This makes the index page"""
     response = client.get("/cicd")
+    assert response.status_code == 200
+    # assert b"Page 3" in response.data
+
+def test_request_pylint(client):
+    """This makes the index page"""
+    response = client.get("/pylint")
+    assert response.status_code == 200
+    # assert b"Page 3" in response.data
+
+def test_request_aaa(client):
+    """This makes the index page"""
+    response = client.get("/aaa")
+    assert response.status_code == 200
+    # assert b"Page 3" in response.data
+
+def test_request_oop(client):
+    """This makes the index page"""
+    response = client.get("/oop")
+    assert response.status_code == 200
+    # assert b"Page 3" in response.data
+
+def test_request_solid(client):
+    """This makes the index page"""
+    response = client.get("/solid")
     assert response.status_code == 200
     # assert b"Page 3" in response.data
 
